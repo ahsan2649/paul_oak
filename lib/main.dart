3e3b859a5345c3d9ed96a58f4a3466530f9ce432
ahsan2649 <ahsan2649@outlook.com>
2024-01-07 13:52:58 +0100

    loading word list from json

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,48 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:paul_oak/pages/home_screen.dart';
 import 'package:paul_oak/pages/practice_screen.dart';
 import 'package:paul_oak/pages/results_screen.dart';
 import 'package:paul_oak/word.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  Future<List<Word>> loadAsset() async {
+    final String response = await rootBundle.loadString('assets/SampleWordList.json');
+    return wordFromJson(response);
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
-    List<Word> wordlist = [
-      Word(original: "original1", translation: "translation1", sentence: "sentence1"),
-      Word(original: "original2", translation: "translation2", sentence: "sentence2"),
-      Word(original: "original3", translation: "translation3", sentence: "sentence3"),
-      Word(original: "original4", translation: "translation4", sentence: "sentence4"),
-      Word(original: "original5", translation: "translation5", sentence: "sentence5"),
-      Word(original: "original6", translation: "translation6", sentence: "sentence6"),
-      Word(original: "original7", translation: "translation7", sentence: "sentence7"),
-      Word(original: "original8", translation: "translation8", sentence: "sentence8"),
-      Word(original: "original9", translation: "translation9", sentence: "sentence9"),
-      Word(original: "original10", translation: "translation10", sentence: "sentence10"),
-      Word(original: "original11", translation: "translation11", sentence: "sentence11"),
-    ];
+
     return MaterialApp(
-      home: Scaffold(body: PracticeScreen(words: wordlist,)),
+      home: Scaffold(
+        body: FutureBuilder(future: loadAsset(), builder: (BuildContext context, AsyncSnapshot<List<Word>> snapshot){
+          Widget child;
+          if (snapshot.hasData) {
+            child = PracticeScreen(words:snapshot.requireData);
+          }
+          else{
+            child = const Text("Loading words...");
+          }
+          return child;
+        }),
+      ),
     );
   }
 }
-
--- lib/word.dart
+++ lib/word.dart
@@ -1,21 +1,36 @@
-class Word{
-  final String original;
-  final String translation;
-  final String sentence;
-  
-  const Word({
-    required this.original,
-    required this.translation,
-    required this.sentence
-  });
+import 'dart:convert';
+
+List<Word> wordFromJson(String str) => List<Word>.from(json.decode(str).map((x) => Word.fromJson(x)));
+
+String wordToJson(List<Word> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Word {
+    String sentence;
+    String translation;
+    String original;
+
+    Word({
+        required this.sentence,
+        required this.translation,
+        required this.original,
+    });
+
+    factory Word.fromJson(Map<String, dynamic> json) => Word(
+        sentence: json["sentence"],
+        translation: json["translation"],
+        original: json["original"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "sentence": sentence,
+        "translation": translation,
+        "original": original,
+    };
 }
 
-class PracticeWord{
+class PracticeWord {
   final Word word;
   int priority;
 
-  PracticeWord({
-    required this.word,
-    required this.priority
-  });
+  PracticeWord({required this.word, required this.priority});
 }
